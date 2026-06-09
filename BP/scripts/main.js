4dd357e80f32c8598bd570435d2f3554de150f47
Dorios Studios <chavez100577@gmail.com>
2026-06-09 00:39:21 -0600

    Removed unused file

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -1,6 +1,4 @@
-import './DoriosAPI/config.js'
-import 'vein_mine.js'
-import 'vein_command.js'
-import 'itemStackClass.js'
-import 'insight_registry_injector.generated.js'
-
+import "./DoriosAPI/config.js";
+import "vein_mine.js";
+import "vein_command.js";
+import "itemStackClass.js";
